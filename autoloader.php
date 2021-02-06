98eced6b07bb9dc4e5fd548751e20c9e97f0a059
Akihito Koriyama <akihito.koriyama@gmail.com>
2021-02-06 11:28:01 +0900

    Update autoloader.php

--- autoloader.php
+++ autoloader.php
@@ -1,18 +1,22 @@
 <?php
 
+declare(strict_types=1);
+
 use Koriym\NullObject\NullObject;
 
-spl_autoload_register(static function (string $className):void {
+spl_autoload_register(static function (string $className): void {
     $hasNullPostfix = substr($className, -4) === 'Null';
     if (! $hasNullPostfix) {
         return;
     }
+
     $interfaceName = substr($className, 0, strlen($className) - 4);
     if (! interface_exists($interfaceName)) {
         return;
     }
-    /** @var string $tmpDir */
+
     $tmpDir = $_ENV['NULL_OBJECT_TMP'] ?? sys_get_temp_dir();
+    assert(is_string($tmpDir));
     $nullClass = new NullObject($tmpDir);
     $file = $nullClass->getNullFilePath($interfaceName);
     if (file_exists($file)) {
@@ -21,5 +25,5 @@ spl_autoload_register(static function (string $className):void {
         return;
     }
 
-    (bool) ($nullClass)($interfaceName);
+    ($nullClass)($interfaceName);
 });
