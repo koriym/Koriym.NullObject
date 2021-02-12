e1168875ebf4df149969211607bbf13a7416a0b1
Akihito Koriyama <akihito.koriyama@gmail.com>
2021-02-12 11:56:01 +0900

    Update demo
    The interface has to be in a separate file.

--- demo/run.php
+++ demo/run.php
@@ -3,14 +3,11 @@
 namespace FooName;
 
 require dirname(__DIR__) . '/vendor/autoload.php';
+$nullLoad = require dirname(__DIR__) . '/autoload.php';
+spl_autoload_register($nullLoad);
 
-$nullLoader = require dirname(__DIR__) . '/autoload.php';
-spl_autoload_register($nullLoader);
+require __DIR__ . '/FooInterface.php';
 
-interface FooInterface
-{
-}
-
-$null = new FooInterfaceNull;
+$null = new FooInterfaceNull();
 
 echo $null instanceof FooInterface ? 'It works!' : 'It does not work.';
